4da082acb28c31641ab71dd2a912b374390d5481
Salvrath <82416041+Salvrath@users.noreply.github.com>
2026-07-28 16:05:03 +0200

    Load portal heading fixes

--- app/portal/layout.tsx
+++ app/portal/layout.tsx
@@ -1,5 +1,6 @@
 import "./portal.css";
 import "./portal-feedback.css";
+import "./portal-layout-fixes.css";
 
 export default function PortalLayout({ children }: { children: React.ReactNode }) {
   return children;
